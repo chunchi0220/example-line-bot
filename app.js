252b5b8b1b2c79231aca5e5a98ddc619bd248e0e
chunchi0220 <5849250@gmail.com>
2023-09-02 01:50:55 +0800

    update file

--- app.js
+++ app.js
@@ -1,5 +1,67 @@
 const linebot = require('linebot'); //引入linebot套件
 const express = require('express'); //引入express套件
+var req = require('request'); //引入request套件
+var rq=require('request-promise')//引入request-promise模組
+const bodyParser = require('body-parser');
+
+// //去讀取google首頁(測試request套件)
+// request('https://www.google.com/',function(error,res,body){
+//     console.log('error:',error);
+//     console.log('statusCode:',res && res.statusCode); //若有回應，呈現狀態碼
+//     console.log('body:',body); //讀取內容本體
+// });
+const SITE_NAME='臺南市';
+const opt={
+    url:"https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/O-A0001-001?Authorization=CWB-6F525D28-7918-4013-BFB4-71227C069D24&downloadType=WEB&format=JSON",
+    json:true //啟用自動解析json
+};
+
+//回乎函式寫法
+// req(opt,function(error,response,body){
+//     if(!error && response.statusCode===200){
+//         var data;
+//         //var town;
+//         for(i in body['cwbopendata']['location']){
+//             if(body['cwbopendata']['location'][i]['parameter'][0]['parameterValue']==SITE_NAME){
+//                 // console.log(body['cwbopendata']['location'][i]['weatherElement'][14]['elementValue']);
+//                 //town=body['cwbopendata']['location'][i]['parameter'][2]['parameterValue'];
+//                 data=body['cwbopendata']['location'][i]['weatherElement'][14]['elementValue']['value'];
+//                 break;
+//             }
+//             //console.log(body['cwbopendata']['location'][i]['parameter'][2]['parameterValue']);
+//         }
+//         console.log(data);
+//     }
+// });
+
+////promise寫法
+// rq(opt)
+// .then(function(res){
+//     var data;
+//     for(i in res['cwbopendata']['location']){
+//         if(res['cwbopendata']['location'][i]['parameter'][0]['parameterValue']==SITE_NAME){
+//             // console.log(body['cwbopendata']['location'][i]['weatherElement'][14]['elementValue']);
+//             data=res['cwbopendata']['location'][i]['weatherElement'][14]['elementValue']['value'];
+//             break;
+//         }
+//     }
+//     console.log(data);
+//             })
+//             .catch(function(error){
+//                 console.log("出錯了～找不到指定資源…");
+//             });
+
+function readweatherAPI(res){
+    var data;
+    for(i in res['cwbopendata']['location']){
+        if(res['cwbopendata']['location'][i]['parameter'][0]['parameterValue']==SITE_NAME){
+            // console.log(body['cwbopendata']['location'][i]['weatherElement'][14]['elementValue']);
+            data=res['cwbopendata']['location'][i]['weatherElement'][14]['elementValue']['value'];
+            break;
+        }
+    }
+    return data;
+}
 
 //建立linebot 物件
 const bot = linebot({
@@ -10,23 +72,64 @@ const bot = linebot({
 });
 
 const app = express();
+// 指定使用ejs樣板引擎
+app.set('view engine','ejs'); 
  
 //驗證數位簽章並解析JSON資料
 const linebotParser = bot.parser();
 
-//處理首頁的請求
-app.get('/',function(req,res){
-    res.send('Hello world');
+//處理首頁的請求(暫時不做，要得懂json檔，在做比較好)
+app.get('/',function(request,response){
+    rq(opt)
+    .then(function(res){
+        response.render('index',{weatherAPI:readweatherAPI(res)}); //把天氣狀況交給ejs引琴合成HTML網頁
+    })
+    .catch(function(error){
+        response.send('無法取得資料');
+    });
 });
 //處理line訊息伺服器的請求
 app.post('/linewebhook', linebotParser);
 
-//處理訊息事件
+// //處理訊息事件(回復一樣的話)
+// bot.on('message', function (event) {
+//   event.reply(event.message.text).then(function (data) {  //回復接收到的訊息文字
+//   }).catch(function (error) {
+//     console.log('Error', error);
+//   });
+// });
 bot.on('message', function (event) {
-  event.reply(event.message.text).then(function (data) {  //回復接收到的訊息文字
-  }).catch(function (error) {
-    console.log('Error', error);
-  });
+	switch (event.message.type) {
+		case 'text':
+			switch (event.message.text) {
+				case '天氣':
+					let data;
+					rq(opt)
+					.then(function (res) {
+						data = readweatherAPI(res);
+						event.reply(data);
+					})
+					.catch(function (err) {
+						event.reply('無法取得資料～');
+					});
+					break;
+
+				case '自我介紹一下':
+					return event.reply('Hello ' + '我是測試用的linebot' + ' ' + '祈');
+					break;
+			}
+			break;
+		case 'sticker':
+			event.reply({
+				type: 'sticker',
+				packageId: 1,
+				stickerId: 1
+			});
+			break;
+		default:
+			event.reply('Unknow message: ' + JSON.stringify(event));
+			break;
+	}
 });
 
 app.listen(process.env.PORT || 80, function () {
